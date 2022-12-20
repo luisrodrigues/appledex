3b53cd776de8db93bd7b8ecac2e7ce62ff754d00
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-20 18:28:09 +0000

    Initial Commit

---
+++ appledex/appledexApp.swift
@@ -0,0 +1,17 @@
+//
+//  appledexApp.swift
+//  appledex
+//
+//  Created by Rodrigues, Luis (EXTERN: EEDS) on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct appledexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
