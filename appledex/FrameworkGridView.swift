c8a99e961f3025160c51b0aff68e0edfd9f6bba2
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-20 21:35:46 +0000

    Finished main screen UI (Grid with Navigation and Scroll view).

--- appledex/FrameworkGridView.swift
+++ appledex/FrameworkGridView.swift
@@ -8,35 +8,50 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+	
+	let columns: [GridItem] = [
+		GridItem(.flexible()),
+		GridItem(.flexible()),
+		GridItem(.flexible())
+	]
+	
 	var body: some View {
-		FrameworkTitleView(
-			name: MockData.sampleFramework.name,
-			imageName: MockData.sampleFramework.imageName)
+		NavigationView {
+			ScrollView {
+				LazyVGrid(columns: columns) {
+					ForEach(MockData.frameworks) { framework in
+						FrameworkTitleView(
+							framework: framework)
+					}
+				}
+			}
+			.navigationTitle("🍎 Frameworks")
+		}
 	}
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
 	static var previews: some View {
 		FrameworkGridView()
+			.preferredColorScheme(.dark)
 	}
 }
 
 struct FrameworkTitleView: View {
-	var name: String
-	var imageName: String
+	var framework: Framework
 	
 	var body: some View {
 		VStack {
-			Image(imageName)
+			Image(framework.imageName)
 				.resizable()
 				.frame(width: 90, height: 90, alignment: .center)
-			Text(name)
+			Text(framework.name)
 				.font(.title2)
 				.fontWeight(.semibold)
 				.scaledToFit()
 				.minimumScaleFactor(0.6)
 		}
-		
+		.padding()
 	}
 	
 }
