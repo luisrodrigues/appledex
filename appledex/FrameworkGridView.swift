cfde10793d24a398b227af65f140a65290cb5340
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-20 18:55:22 +0000

    Created grid item View

--- appledex/FrameworkGridView.swift
+++ appledex/FrameworkGridView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	var body: some View {
+		FrameworkTitleView(
+			name: MockData.sampleFramework.name,
+			imageName: MockData.sampleFramework.imageName)
+	}
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
-    static var previews: some View {
-        FrameworkGridView()
-    }
+	static var previews: some View {
+		FrameworkGridView()
+	}
+}
+
+struct FrameworkTitleView: View {
+	var name: String
+	var imageName: String
+	
+	var body: some View {
+		VStack {
+			Image(imageName)
+				.resizable()
+				.frame(width: 90, height: 90, alignment: .center)
+			Text(name)
+				.font(.title2)
+				.fontWeight(.semibold)
+				.scaledToFit()
+				.minimumScaleFactor(0.6)
+		}
+		
+	}
+	
 }
